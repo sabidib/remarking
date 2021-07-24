0375ec1d2b7a398c52d04c7d9f15efd25c742604
Samy Abidib <abidibs@gmail.com>
2021-07-24 10:46:41 -0400

    Add master doc var

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -26,7 +26,10 @@ copyright = '2021, Samy Abidib'
 author = 'Samy Abidib'
 
 # The full version, including alpha/beta/rc tags
-release = '0.1.0'
+release = '0.1.1'
+
+master_doc = 'index'
+
 
 
 # -- General configuration ---------------------------------------------------
